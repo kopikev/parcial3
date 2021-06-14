51bb8492302c2a62aa1eee36791606fc3fb77cd7
kopikev <79229819+kopikev@users.noreply.github.com>
2021-06-13 19:08:02 -0500

    Add files via upload

---
+++ parcial3/js/script.js
@@ -0,0 +1,13 @@
+/* Activador de jQuery */
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        speed: 300,
+        fade: false,
+        arrow: true,
+        pauseOnHover: false,
+      }); 
+});
